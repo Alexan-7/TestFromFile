d22e594f98563c8eedb44d04cf717b784606aa2a
Sasha <brdgn@bk.ru>
2023-09-13 13:01:15 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TestFromFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(50, 81);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(167, 29);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(50, 144);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(167, 29);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(50, 209);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(167, 29);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(102, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(345, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(646, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 43);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1271, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Тест знаний студента";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿// Программа тестирует студента по какому-либо предмету обучения
+using System;
+using System.Windows.Forms;
+// Другие директивы using удалены, поскольку они не используются
+// в данной программе
+
+namespace TestFromFile
+{
+    public partial class Form1 : Form
+    {
+        int СчетВопросов = 0; // Счет вопросов
+        int ПравилОтветов = 0; // Количество правильных ответов
+        int НеПравилОтветов = 0; // Количество неправильных ответов
+        string[] НеПравилОтветы; // Массив вопросов,
+                                 // на которые даны неправильные ответы
+        int НомерПравОтвета; // Номер правильного ответа
+        int ВыбранОтвет; // Номер ответа, выбранный студентом
+                         // Чтобы русские буквы читались корректно, объявляем объект Кодировка:
+        System.Text.Encoding Кодировка =
+        System.Text.Encoding.GetEncoding("UTF-8");
+        System.IO.StreamReader Читатель;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Следующий вопрос";
+            button2.Text = "Выход";
+            // Подписка на событие изменение состояния
+            // переключателей RadioButton:
+            radioButton1.CheckedChanged +=
+            new System.EventHandler(ИзмСостПерекл);
+            radioButton2.CheckedChanged +=
+            new System.EventHandler(ИзмСостПерекл);
+            radioButton3.CheckedChanged +=
+            new System.EventHandler(ИзмСостПерекл);
+            НачалоТеста();
+        }
+        void НачалоТеста()
+        {
+            try
+            { // Создание экземпляра StreamReader для чтения из файла
+                Читатель = new System.IO.
+                    StreamReader(System.IO.Directory.GetCurrentDirectory()
+                                + @"\test.qst", Кодировка);
+                this.Text = Читатель.ReadLine(); // Название предмета
+                                                 // Обнуление всех счетчиков:
+                СчетВопросов = 0; ПравилОтветов = 0; НеПравилОтветов = 0;
+                НеПравилОтветы = new string[100];
+            }
+            catch (Exception Ситуация)
+            { // Отчет о всех ошибках
+                MessageBox.Show(Ситуация.Message, "Ошибка",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            ЧитатьСледВопрос();
+        }
+        void ЧитатьСледВопрос()
+        {
+            label1.Text = Читатель.ReadLine();
+            // Считывание вариантов ответа:
+            radioButton1.Text = Читатель.ReadLine();
+            radioButton2.Text = Читатель.ReadLine();
+            radioButton3.Text = Читатель.ReadLine();
+            // Выясняем, какой ответ - правильный:
+            НомерПравОтвета = int.Parse(Читатель.ReadLine());
+            // Переводим все переключатели в состояние "выключено":
+            radioButton1.Checked = false; radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            // Первая кнопка не активна, пока студент не выберет вариант ответа
+            button1.Enabled = false;
+            СчетВопросов = СчетВопросов + 1; // Проверка, конец ли файла:
+            if (Читатель.EndOfStream == true) button1.Text = "Завершить";
+        }
+        private void ИзмСостПерекл(object sender, EventArgs e)
+        { // Кнопка "Следующий вопрос" становится активной, и ей передаем фокус
+            button1.Enabled = true; button1.Focus();
+            RadioButton Переключатель = (RadioButton)sender;
+            string tmp = Переключатель.Name;
+            // Выясняем номер ответа, выбранный студентом:
+            ВыбранОтвет = int.Parse(tmp.Substring(11));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Щелчок на кнопке
+            // "Следующий вопрос/Завершить/Начать тестирование сначала".
+            // Счет правильных ответов:
+            if (ВыбранОтвет == НомерПравОтвета)
+                ПравилОтветов = ПравилОтветов + 1;
+            if (ВыбранОтвет != НомерПравОтвета)
+            { // Счет неправильных ответов:
+                НеПравилОтветов = НеПравилОтветов + 1;
+                // Запоминаем вопросы с неправильными ответами:
+                НеПравилОтветы[НеПравилОтветов] = label1.Text;
+            }
+            if (button1.Text == "Начать тестирование сначала")
+            {
+                button1.Text = "Следующий вопрос";
+                // Переключатели становятся видимыми, доступными для выбора:
+                radioButton1.Visible = true; radioButton2.Visible = true;
+                radioButton3.Visible = true;
+                // Переход к началу файла
+                НачалоТеста(); return;
+            }
+            if (button1.Text == "Завершить")
+            {
+                Читатель.Close(); // Закрываем текстовый файл
+                                  // Переключатели делаем невидимыми:
+                radioButton1.Visible = false; radioButton2.Visible = false;
+                radioButton3.Visible = false;
+                // Формируем оценку за тест:
+                label1.Text = string.Format("Тестирование завершено.\n" +
+                "Правильных ответов: {0} из {1}.\n" +
+                "Оценка в пятибалльной системе: {2:F2}.", ПравилОтветов,
+                СчетВопросов, (ПравилОтветов * 5F) / СчетВопросов);
+                // 5F - это максимальная оценка
+                button1.Text = "Начать тестирование сначала";
+                // Вывод вопросов, на которые Вы дали неправильный ответ
+                string Str = "СПИСОК ВОПРОСОВ, НА КОТОРЫЕ ВЫ ДАЛИ " +
+                "НЕПРАВИЛЬНЫЙ ОТВЕТ:\n\n";
+                for (int i = 1; i <= НеПравилОтветов; i++)
+                    Str = Str + НеПравилОтветы[i] + "\n";
+                // Если есть неправильные ответы, то вывести через MessageBox
+                // список соответствующих вопросов:
+                if (НеПравилОтветов != 0)
+                    MessageBox.Show(Str, "Тестирование завершено");
+            }
+            if (button1.Text == "Следующий вопрос") ЧитатьСледВопрос();
+        }
+
+            private void button2_Click(object sender, EventArgs e)
+        {
+            // Щелчок на кнопке "Выход"
+            this.Close();
+        }
+    }
+}
